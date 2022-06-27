cf90b11502ea9f9f6c45ccff0e1e0cee296538dd
Rafael Victor <rafaelv.boscato@hotmail.com>
2022-06-27 14:03:28 -0300

    ♻️ refactoring code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConsumerModule } from './consumer/consumer.module';
 import { Constants, KafkaConfig } from './kafka.config';
-import { LoggerModule } from './logger/logger.module';
 
 const kafkaImport = ClientsModule.register([
   KafkaConfig(Constants.KafkaClientToken),
 ]);
 
 @Module({
-  imports: [LoggerModule, kafkaImport,ConsumerModule],
+  imports: [ kafkaImport,ConsumerModule],
   controllers: [AppController],
   providers: [AppService],
 })
